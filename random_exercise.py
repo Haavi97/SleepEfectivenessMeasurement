7e7e4ba32e8f0a42511437d7f4f5de70e744d292
Haavi <jortin@easyfeedbacktoken.io>
2024-02-24 12:26:59 +0200

    Error handling added in generate_random_int_places

--- random_exercise.py
+++ random_exercise.py
@@ -1,10 +1,18 @@
 import random
 
 
-def generate_random_int_places(n: int) -> int:
-    # Generates a random integer with maximum n places
-    return random.randint(0, 10**n)
+def generate_random_int_places(n: int = 3) -> int:
+    # Generates a random integer with maximum n places. 
+    # n=3 by default. On failure 3 is used.
+    try:
+        return random.randint(0, 10**n)
+    except:
+        print(
+            'Error in generate_random_int_places.\n'
+            f'Returning with n=3.\nGiven n: {n}\n'
+            f'n type: {type(n)}')
+        return random.randint(0, 10**3)
 
 
 if __name__ == '__main__':
-    print(generate_random_int_places(3))
+    print(generate_random_int_places('2'))
